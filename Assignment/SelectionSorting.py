982ddb92242a4877d75324bd0d14fa31523dc09a
Nirmal Bam <codernirmalbam@gmail.com>
2025-10-06 13:23:54 +0545

    Implement selection sort algorithm in Python

---
+++ Assignment/SelectionSorting.py
@@ -0,0 +1,13 @@
+def selection_sort (arr) :
+    n = len(arr)
+    for i in range(n-1) :
+        min_index = i
+        for j in range(i+1 , n) :
+            if arr[j] < arr[min_index] :
+                min_index = j
+        arr[i] , arr[min_index] = arr[min_index] , arr[i]
+
+    return arr 
+
+nums = [4,7,1,9,14,2,4,3]
+print(selection_sort(nums))
